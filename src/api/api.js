3eff21120dc2946ce1475fc2df643ee3b5d6a895
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2024-11-28 19:45:46 -0600

    [Add]: Change to prod

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // baseURL: "http://18.208.163.231:8000",
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://18.208.163.231:8000",
+  // baseURL: "http://localhost:8000/api",
 });
 
 export default apiClient;
